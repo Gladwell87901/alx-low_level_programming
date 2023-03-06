9de343b976612e4bd585068ebe0fb3d630349e82
Gladwell87901 <geogladwell@gmail.com>
2023-03-06 13:01:43 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * A program that prints a line using printf
+ * return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
