db845d4366c031908801f35e3b45c10f457b71fe
Gladwell87901 <geogladwell@gmail.com>
2023-03-06 12:45:31 -0800

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *A program that prints using puts()
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
-	return 0;
+	return (0);
 }
